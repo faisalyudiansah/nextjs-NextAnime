6d28446a451ec207529589873f4d784e64f155f2
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2023-12-29 05:16:29 +0700

    fix: fixed card anime list

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import CardAnimeList from "./components/CardAnimeList";
+import CardAnimeList from "@/components/CardAnimeList";
 import Link from 'next/link'
 
 export default async function Home() {
@@ -8,15 +8,19 @@ export default async function Home() {
 
   return (
     <>
-      <div className="flex justify-between p-4">
-        <div>Popular anime</div>
-        <Link href={'/populer'} className="btn p-3 bg-[tomato] rounded-xl">Popular anime</Link>
+      <div className="flex justify-between items-center md:m-10 m-4">
+        <h1 className="font-serif font-bold md:text-2xl text-lg">Popular anime</h1>
+        <Link
+          href={'/populer'}
+          className="text-white text-sm p-2 bg-orange-600 hover:bg-orange-500 rounded-xl">
+          Popular anime
+        </Link>
       </div>
-      <div className='m-4 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4'>
+      <section className='md:m-10 m-4 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4'>
         {animeList.data.map((anime, i) => {
           return <CardAnimeList key={i} anime={anime} />
         })}
-      </div>
+      </section>
     </>
   )
 }
---
+++ src/components/CardAnimeList/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+
+const CardAnimeList = ({ anime }) => {
+  return (
+    <div className="card bg-orange-300 border shadow-md rounded-xl flex flex-col">
+      <img
+        className="w-full md:max-h-60 max-h-52 rounded-xl object-cover"
+        src={anime.images.webp.image_url}
+        alt="image"
+      />
+      <div className="p-4 flex flex-col justify-between flex-1">
+        <h4 className="text-xl font-semibold tracking-tight">{anime.title}</h4>
+        <div className="mt-3 max-w-52 mb-1">
+          <p className="mb-4 leading-normal">Rating : {anime.score}</p>
+          <Link
+            href={`/detail-anime/${anime.mal_id}`}
+            className="px-4 py-2 text-center text-sm text-white bg-orange-600 hover:bg-orange-500 rounded-lg shadow">
+            Detail
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardAnimeList;
