8b6f5d0829d6dc9fdf5d6bdcfea871b5794f1c77
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2023-12-29 04:15:36 +0700

    fix: fixed card anime list

--- src/app/components/CardAnimeList/index.js
+++ src/app/components/CardAnimeList/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 
 const CardAnimeList = ({ anime }) => {
   return (
-    <>
-      <div className="bg-base-100 shadow-lg border">
-        <img src={anime.images.webp.image_url} alt="anime_image" />
-        <h2 className="card-title">{anime.title}</h2>
-        <p>Rating : {anime.score}</p>
+    <div className="bg-base-100 shadow-lg border overflow-hidden">
+      <img
+        className="w-full max-h-48"
+        src={anime.images.webp.image_url}
+        alt={`Image of ${anime.title}`}
+      />
+      <div className="p-4">
+        <h2 className="font-bold text-xl mb-2">{anime.title}</h2>
+        <p className="text-gray-600">Rating: {anime.score}</p>
+        <Link href={`/detail-anime/${anime.mal_id}`} className="btn bg-[tomato] inline-block mt-2">
+          Detail
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
 'use client'
 import CardAnimeList from "./components/CardAnimeList";
+import Link from 'next/link'
 
 export default async function Home() {
-  let response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/top/anime')
+  let response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/top/anime?limit=8')
   let animeList = await response.json()
 
   return (
     <>
-      <div>Popular anime</div>
-      <div className='grid grid-cols-3 gap-4'>
+      <div className="flex justify-between p-4">
+        <div>Popular anime</div>
+        <Link href={'/populer'} className="btn p-3 bg-[tomato] rounded-xl">Popular anime</Link>
+      </div>
+      <div className='m-4 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4'>
         {animeList.data.map((anime, i) => {
           return <CardAnimeList key={i} anime={anime} />
         })}
